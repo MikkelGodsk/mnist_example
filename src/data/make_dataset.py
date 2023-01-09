ab1c1b83a632e520429290ca13edfbc0b4cadf4a
Mikkel <s184399@student.dtu.dk>
2023-01-09 15:12:30 +0100

    Data tests

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,13 +12,13 @@ def mnist(path='data', batch_size=16):
                 torch.tensor(train_raw["labels"], dtype=torch.int64),
             )
             for train_raw in [
-                np.load(os.path.join(path, "processed\\train_{:d}.npz".format(i))) for i in range(8)
+                np.load(os.path.join(path, "processed", "train_{:d}.npz".format(i))) for i in range(8)
             ]
         ]
     )
     train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
 
-    test_raw = np.load(os.path.join(path, "processed\\test.npz"))
+    test_raw = np.load(os.path.join(path, "processed", "test.npz"))
     test_set = torch.utils.data.TensorDataset(
         torch.tensor(test_raw["images"], dtype=torch.float64),
         torch.tensor(test_raw["labels"], dtype=torch.int64),
